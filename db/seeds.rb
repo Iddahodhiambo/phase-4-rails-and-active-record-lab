b28082e971503fa91f99a96faeceeca4ce179573
Iddahodhiambo <iddahawuor@yahoo.com>
2023-04-26 23:01:47 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name:"awiti",last_name:"Luke",grade:90)
+Student.create(first_name:"lindz",last_name:"otieno",grade:67)
